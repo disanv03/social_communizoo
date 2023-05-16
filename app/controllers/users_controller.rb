03ab35d5d9e2149f5e47c66d1918fba2e0aa5e07
disanv <disanv03@gmail.com>
2023-05-16 15:37:22 +0200

    Setting up project

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+       user_params = params.require(:user).permit(:username, :email, :password, :password_confirmation)
+       @user = User.new(user_params)
+       if @user.valid?
+        render 'new'
+       else
+        render 'new'
+       end
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+    has_secure_password
+    has_secure_token :confirmation_token
+
+
+    validates :username,
+    format: {with: /\A[a-zA-Z0-9_]{2,20}\z/, message: 'ne doit contenir que des caractères alphanumériques ou des _'},
+    uniqueness: {case_sensitive: false}
+    validates :email,
+    format: {with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/},
+    uniqueness: {case_sensitive: false}
+    # validates :avatar_file, file: {ext: [:jpg, :png]}
+
+end
